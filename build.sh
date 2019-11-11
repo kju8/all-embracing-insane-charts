c0f55d79b2b3b765a7b5aedf631901aa7309fe97
Takuya Namba <1994athame@gmail.com>
2019-11-11 23:44:07 +0900

    fix builder

--- build.sh
+++ build.sh
@@ -8,6 +8,15 @@ git config --local user.name "GitHub Action"
 
 git remote add origin https://kju8:${GITHUB_TOKEN}@github.com/kju8/all-embracing-insane-charts.git
 
+git checkout master
+git pull origin gh-pages
+
+cd ../
+npm ci
+npm run build --if-present
+
+cd build
+
 git add table.json
 git commit -m "build"
 
