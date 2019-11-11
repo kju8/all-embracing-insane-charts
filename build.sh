3faf636cd26f77281be2390356489884b0604488
Takuya Namba <1994athame@gmail.com>
2019-11-11 20:31:17 +0900

    fix builder

--- build.sh
+++ build.sh
@@ -6,8 +6,9 @@ git init
 git config --local user.email "action@github.com"
 git config --local user.name "GitHub Action"
 
+git remote add origin https://kju8:${GITHUB_TOKEN}@github.com/kju8/all-embracing-insane-charts.git
+
 git add table.json
 git commit -m "build"
 
-git remote set-url origin https://kju8:${GITHUB_TOKEN}@github.com/kju8/all-embracing-insane-charts.git
-git push origin build
+git push origin master:build
